b500d0bfcc363d4a4adaa21e799f845d3d2806e5
FlutLab.io <>
2021-05-19 17:49:07 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MiLoginApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.light,
-        primarySwatch: Colors.pink[200],
+        primarySwatch: Colors.cyan,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       darkTheme: ThemeData(
